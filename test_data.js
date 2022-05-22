ae0d09f2e32e56ea27276929b0e7e6a125dac841
dsszymanski <dan1993us@yahoo.com>
2022-05-22 02:44:26 -0400

    Added js code to generate web page from test data.

---
+++ test_data.js
@@ -0,0 +1,12 @@
+const data = [
+    "test_location_0,test_phone_#_8,test_description_8",
+    "test_location_1,test_phone_#_9,test_description_9",
+    "test_location_2,test_phone_#_2,test_description_2",
+    "test_location_2,test_phone_#_7,test_description_7",
+    "test_location_1,test_phone_#_4,test_description_4",
+    "test_location_0,test_phone_#_5,test_description_5",
+    "test_location_0,test_phone_#_6,test_description_6",
+    "test_location_1,test_phone_#_1,test_description_1",
+    "test_location_2,test_phone_#_3,test_description_3",
+    "test_location_0,test_phone_#_0,test_description_0",
+]
