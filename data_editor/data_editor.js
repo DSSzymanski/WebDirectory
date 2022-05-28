cbb3d916eaf4b78ae681957d53c097250d42dab3
dsszymanski <dan1993us@yahoo.com>
2022-05-28 03:20:48 -0400

    Added js code to generate new remove boxes with filled select bars when the related button is clicked.

--- data_editor/data_editor.js
+++ data_editor/data_editor.js
@@ -1,20 +1,48 @@
-const generateNewRemoveBox = () => {
+const generateNewRemoveBox = (data) => {
     const remove_container = document.getElementById("remove-container");
+
+    //modify new box into DOM
     let new_box = document.createElement("div");
-    new_box.innerText = "test";
     remove_container.appendChild(new_box);
+
+    //setup box
+    new_box.className = "remove-box";
+
+    //add select element to remove box
+    let select_entry = document.createElement("select");
+    new_box.appendChild(select_entry);
+
+    //initial blank option
+    let init_option_element = document.createElement("option");
+    init_option_element.innerText = "Select phone entry to remove.";
+    select_entry.appendChild(init_option_element);
+
+    //populate select element
+    for (const phone_entry of data) {
+        let option_element = document.createElement("option");
+        option_element.innerText = phone_entry;
+        select_entry.appendChild(option_element);
+    }
+
+    //add button to remove add box
+    let remove_box_btn = document.createElement("button");
+    remove_box_btn.innerText = "-";
+    remove_box_btn.onclick = () => {
+        remove_container.removeChild(new_box);
+    };
+    new_box.appendChild(remove_box_btn);
 }
 
 const generateNewAddBox = () => {
     const add_container = document.getElementById("add-container");
-    
+
     //modify new box into DOM
     let new_box = document.createElement("div");
     add_container.appendChild(new_box);
 
     //setup box
     new_box.className = "add-box";
-    
+
     //add location textbox
     new_box.appendChild(generateTextInputElement("Location/Unit"));
 
