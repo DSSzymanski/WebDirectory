46c58a226b047b408dd0513d701650437805b4b4
dsszymanski <dan1993us@yahoo.com>
2022-05-27 22:31:35 -0400

    Added js code to handle the new elements used to add new numbers to the generated code.

--- data_editor/data_editor.js
+++ data_editor/data_editor.js
@@ -7,7 +7,35 @@ const generateNewRemoveBox = () => {
 
 const generateNewAddBox = () => {
     const add_container = document.getElementById("add-container");
+    
+    //modify new box into DOM
     let new_box = document.createElement("div");
-    new_box.innerText = "test";
     add_container.appendChild(new_box);
+
+    //setup box
+    new_box.className = "add-box";
+    
+    //add location textbox
+    new_box.appendChild(generateTextInputElement("Location/Unit"));
+
+    //add phone number textbox
+    new_box.appendChild(generateTextInputElement("Phone Number"));
+
+    //add description textbox
+    new_box.appendChild(generateTextInputElement("Description"));
+
+    //add button to remove add box
+    let remove_box_btn = document.createElement("button");
+    remove_box_btn.innerText = "-";
+    remove_box_btn.onclick = () => {
+        add_container.removeChild(new_box);
+    };
+    new_box.appendChild(remove_box_btn);
+}
+
+const generateTextInputElement = (placeholder) => {
+    let new_text_input = document.createElement("input");
+    new_text_input.setAttribute("type", "text");
+    new_text_input.setAttribute("placeholder", placeholder);
+    return new_text_input;
 }
